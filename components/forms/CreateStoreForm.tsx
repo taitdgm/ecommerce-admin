e8ddd30e3cf7cf9184e4e6bbdcf26d1aaf37ea52
taitd-gms <taitd.gm@gmail.com>
2023-08-23 12:33:39 +0700

    feat: add prisma

--- components/forms/CreateStoreForm.tsx
+++ components/forms/CreateStoreForm.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+import axios from "axios";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -14,10 +16,13 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { toast } from "react-hot-toast";
 
 type CreateStoreFormInput = z.infer<typeof createStoreSchema>;
 
 const CreateStoreForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const form = useForm<CreateStoreFormInput>({
     resolver: zodResolver(createStoreSchema),
     defaultValues: {
@@ -25,8 +30,17 @@ const CreateStoreForm = () => {
     },
   });
 
-  const onSubmit = (values: CreateStoreFormInput) => {
-    console.log(values);
+  const onSubmit = async (values: CreateStoreFormInput) => {
+    try {
+      setIsLoading(true);
+      const response = await axios.post("/api/stores", values);
+      toast.success("Store created successfully");
+      window.location.assign(`/${response.data.id}`);
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -45,7 +59,9 @@ const CreateStoreForm = () => {
             </FormItem>
           )}
         />
-        <Button type="submit">Create</Button>
+        <Button type="submit" disabled={isLoading}>
+          Create
+        </Button>
       </form>
     </Form>
   );
