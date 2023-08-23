950245835a9f01dd86431a155892c49df7a977ce
taitd-gms <taitd.gm@gmail.com>
2023-08-23 11:50:04 +0700

    feat: add create store modal

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,7 +1,19 @@
 "use client";
 
+import { useCreateStoreModal } from "@/hooks/useCreateStoreModal";
+import { useEffect } from "react";
+
 const RootPage = () => {
-  return <div>RootPage</div>;
+  const isOpen = useCreateStoreModal((state) => state.isOpen);
+  const onOpen = useCreateStoreModal((state) => state.onOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return null;
 };
 
 export default RootPage;
---
+++ components/forms/CreateStoreForm.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { createStoreSchema } from "@/validators/store";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+type CreateStoreFormInput = z.infer<typeof createStoreSchema>;
+
+const CreateStoreForm = () => {
+  const form = useForm<CreateStoreFormInput>({
+    resolver: zodResolver(createStoreSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = (values: CreateStoreFormInput) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Store name</FormLabel>
+              <FormControl>
+                <Input placeholder="GM Store" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Create</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default CreateStoreForm;
